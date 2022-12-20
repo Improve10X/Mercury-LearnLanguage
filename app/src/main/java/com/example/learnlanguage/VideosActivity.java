919f3359d4ee17d9c9bb6f3f3eaa887bbbe5087b
K Hareesh <hareesh.komma@improve10x.com>
2022-12-20 16:19:19 +0530

    we created SplashActivityScreen and VideosActivity

---
+++ app/src/main/java/com/example/learnlanguage/VideosActivity.java
@@ -0,0 +1,15 @@
+package com.example.learnlanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+        getSupportActionBar().setTitle("Videos");
+    }
+}
