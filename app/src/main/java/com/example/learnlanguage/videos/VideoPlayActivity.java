bf169cfeb656449458945d67da8581aa4e794be6
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2022-12-22 11:48:41 +0530

    we designed xml file in VideoPlayActivity.

--- app/src/main/java/com/example/learnlanguage/videos/VideoPlayActivity.java
+++ app/src/main/java/com/example/learnlanguage/videos/VideoPlayActivity.java
@@ -1,8 +1,10 @@
-package com.example.learnlanguage;
+package com.example.learnlanguage.videos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.learnlanguage.R;
 
 public class VideoPlayActivity extends AppCompatActivity {
 
